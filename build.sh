f88144c79fbe4b3db80534dabc68f7f44e945934
mileoa <ezkimozzzzz@gmail.com>
2024-12-25 20:10:26 +1000

    test

--- build.sh
+++ build.sh
@@ -5,15 +5,11 @@ set -o errexit
 # Install uv
 pip install uv
 
-
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 uv sync
 
 # Convert static asset files
-python manage.py collectstatic --no-input
+uv run python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python manage.py migrate
-
-# Install gunicorn
-pip install gunicorn uvicorn
+uv run python manage.py migrate
