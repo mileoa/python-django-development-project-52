520a32ce14ccc269b273729f7242308b24afe4d8
mileoa <ezkimozzzzz@gmail.com>
2024-12-27 20:22:11 +1000

    test

--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 #pip install -r requirements.txt
 pip install uv
-pip install
 uv sync
 # Convert static asset files
 uv run python manage.py collectstatic --no-input
