0988bdfe29c0d9a4e5713a4a8c8c610c982c2627
mileoa <ezkimozzzzz@gmail.com>
2024-12-27 20:20:47 +1000

    test

--- build.sh
+++ build.sh
@@ -3,10 +3,12 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r requirements.txt
-
+#pip install -r requirements.txt
+pip install uv
+pip install
+uv sync
 # Convert static asset files
-python manage.py collectstatic --no-input
+uv run python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python manage.py migrate
+uv run python manage.py migrate
