ba9c1b0036915b1f0b228e1b80cc1e462d292f64
mileoa <ezkimozzzzz@gmail.com>
2025-01-01 23:40:55 +1000

    feature: add flash logout message

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 from django.shortcuts import render
 from django.urls import reverse_lazy
 
@@ -16,4 +17,7 @@ class CustomLoginView(SuccessMessageMixin, LoginView):
 
 
 class CustomLogoutView(SuccessMessageMixin, LogoutView):
-    success_message = "Вы разлогинены"
+
+    def dispatch(self, request, *args, **kwargs):
+        messages.add_message(request, messages.INFO, "Вы разлогинены")
+        return super().dispatch(request, *args, **kwargs)
